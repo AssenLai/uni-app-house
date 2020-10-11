86c6bcddc5e66cfe6643ac0613505b7ddaadc4ab
AssenLai <63274690+AssenLai@users.noreply.github.com>
2020-10-12 00:49:26 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App'
+import uniSwiperDot from "@/components/uni-swiper-dot/uni-swiper-dot.vue"
+import uniDrawer from "@/components/uni-drawer/uni-drawer.vue"
+import uniGrid from "@/components/uni-grid/uni-grid.vue"
+import uniGridItem from "@/components/uni-grid-item/uni-grid-item.vue"
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+export default {
+    components: {uniSwiperDot,uniDrawer,uniGrid,uniGridItem}
+}
+const app = new Vue({
+	...App
+})
+app.$mount()
